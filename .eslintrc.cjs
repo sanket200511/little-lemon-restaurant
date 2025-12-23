c34e2cf418868f1b1bbce8f1033e4b75f8bcaf0c
Sanket Kurve <sanketkurve.2005@gmail.com>
2025-12-23 09:42:57 +0530

    fixed some issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 /* eslint-env node */
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react-refresh/only-export-components": "warn",
     "quotes": [2, "double", { "avoidEscape": true }],
     "semi": [2, "always"],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
