0ec4a3fcfad18b9ba5b424f06df3bb56d0e59ec9
Sanket Kurve <sanketkurve.2005@gmail.com>
2025-12-23 09:36:17 +0530

    fixed some issues

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Routes from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/little-lemon">
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Routes />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
